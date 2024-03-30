7a581fb348371f0549c17f681da62c10b9ed9440
CHunYenc <op84a2546@gmail.com>
2024-03-30 20:13:40 +0000

    format: download.py

--- download.py
+++ download.py
@@ -16,102 +16,104 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
+
 def download(url):
-  encode = 0 #不轉檔
-  action = input('要轉檔嗎?[y/n]')
-  if action.lower() == 'y':
-    action = input('選擇轉檔方案[1:僅轉換格式(默認,推薦) 2:NVIDIA GPU 轉檔 3:CPU 轉檔]')
-    if action == '2':
-       encode = 2 #GPU轉檔
-    elif action == '3':
-       encode = 3 #CPU轉檔
+    encode = 0  # 不轉檔
+    action = input('要轉檔嗎?[y/n]')
+    if action.lower() == 'y':
+        action = input('選擇轉檔方案[1:僅轉換格式(默認,推薦) 2:NVIDIA GPU 轉檔 3:CPU 轉檔]')
+        if action == '2':
+            encode = 2  # GPU轉檔
+        elif action == '3':
+            encode = 3  # CPU轉檔
+        else:
+            encode = 1  # 快速無損轉檔
+
+    print('正在下載影片: ' + url)
+    # 建立番號資料夾
+    urlSplit = url.split('/')
+    dirName = urlSplit[-2]
+    if os.path.exists(f'{dirName}/{dirName}.mp4'):
+        print('番號資料夾已存在, 跳過...')
+        return
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+    folderPath = os.path.join(os.getcwd(), dirName)
+    # 配置Selenium參數
+    options = Options()
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--headless')
+    options.add_argument(
+        "user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
+    # 判斷系統為 linux or windows, 執行 chromedriver.exe 的路徑
+    if os.name == 'nt':
+        # Windows
+        service = Service('chromedriver.exe')
+        dr = webdriver.Chrome(options=options)
     else:
-       encode = 1 #快速無損轉檔
+        # Linux
+        service = Service('/usr/bin/chromedriver')
+        dr = webdriver.Chrome(options=options, service=service)
+    dr.get(url)
+    result = re.search("https://.+m3u8", dr.page_source)
+    print(f'result: {result}')
+    m3u8url = result[0]
+    print(f'm3u8url: {m3u8url}')
 
-  print('正在下載影片: ' + url)
-  # 建立番號資料夾
-  urlSplit = url.split('/')
-  dirName = urlSplit[-2]
-  if os.path.exists(f'{dirName}/{dirName}.mp4'):
-    print('番號資料夾已存在, 跳過...')
-    return
-  if not os.path.exists(dirName):
-      os.makedirs(dirName)
-  folderPath = os.path.join(os.getcwd(), dirName)
-  #配置Selenium參數
-  options = Options()
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--disable-extensions')
-  options.add_argument('--headless')
-  options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36")
-  # 判斷系統為 linux or windows, 執行 chromedriver.exe 的路徑
-  if os.name == 'nt':
-      # Windows
-      service = Service('chromedriver.exe')
-      dr = webdriver.Chrome(options=options)
-  else:
-      # Linux
-      service = Service('/usr/bin/chromedriver')
-      dr = webdriver.Chrome(options=options, service=service)
-  dr.get(url)
-  result = re.search("https://.+m3u8", dr.page_source)
-  print(f'result: {result}')
-  m3u8url = result[0]
-  print(f'm3u8url: {m3u8url}')
+    # 得到 m3u8 網址
+    m3u8urlList = m3u8url.split('/')
+    m3u8urlList.pop(-1)
+    downloadurl = '/'.join(m3u8urlList)
 
-  # 得到 m3u8 網址
-  m3u8urlList = m3u8url.split('/')
-  m3u8urlList.pop(-1)
-  downloadurl = '/'.join(m3u8urlList)
+    # 儲存 m3u8 file 至資料夾
+    m3u8file = os.path.join(folderPath, dirName + '.m3u8')
+    urllib.request.urlretrieve(m3u8url, m3u8file)
 
-  # 儲存 m3u8 file 至資料夾
-  m3u8file = os.path.join(folderPath, dirName + '.m3u8')
-  urllib.request.urlretrieve(m3u8url, m3u8file)
+    # 得到 m3u8 file裡的 URI和 IV
+    m3u8obj = m3u8.load(m3u8file)
+    m3u8uri = ''
+    m3u8iv = ''
 
-  # 得到 m3u8 file裡的 URI和 IV
-  m3u8obj = m3u8.load(m3u8file)
-  m3u8uri = ''
-  m3u8iv = ''
+    for key in m3u8obj.keys:
+        if key:
+            m3u8uri = key.uri
+            m3u8iv = key.iv
 
-  for key in m3u8obj.keys:
-      if key:
-          m3u8uri = key.uri
-          m3u8iv = key.iv
+    # 儲存 ts網址 in tsList
+    tsList = []
+    for seg in m3u8obj.segments:
+        tsUrl = downloadurl + '/' + seg.uri
+        tsList.append(tsUrl)
 
-  # 儲存 ts網址 in tsList
-  tsList = []
-  for seg in m3u8obj.segments:
-      tsUrl = downloadurl + '/' + seg.uri
-      tsList.append(tsUrl)
+    # 有加密
+    if m3u8uri:
+        m3u8keyurl = downloadurl + '/' + m3u8uri  # 得到 key 的網址
+        # 得到 key的內容
+        response = requests.get(m3u8keyurl, headers=headers, timeout=10)
+        contentKey = response.content
 
-  # 有加密
-  if m3u8uri:
-      m3u8keyurl = downloadurl + '/' + m3u8uri  # 得到 key 的網址
-      # 得到 key的內容
-      response = requests.get(m3u8keyurl, headers=headers, timeout=10)
-      contentKey = response.content
+        vt = m3u8iv.replace("0x", "")[:16].encode()  # IV取前16位
 
-      vt = m3u8iv.replace("0x", "")[:16].encode()  # IV取前16位
+        ci = AES.new(contentKey, AES.MODE_CBC, vt)  # 建構解碼器
+    else:
+        ci = ''
 
-      ci = AES.new(contentKey, AES.MODE_CBC, vt)  # 建構解碼器
-  else:
-      ci = ''
+    # 刪除m3u8 file
+    deleteM3u8(folderPath)
 
-  # 刪除m3u8 file
-  deleteM3u8(folderPath)
+    # 開始爬蟲並下載mp4片段至資料夾
+    prepareCrawl(ci, folderPath, tsList)
 
-  # 開始爬蟲並下載mp4片段至資料夾
-  prepareCrawl(ci, folderPath, tsList)
+    # 合成mp4
+    mergeMp4(folderPath, tsList)
 
-  # 合成mp4
-  mergeMp4(folderPath, tsList)
+    # 刪除子mp4
+    deleteMp4(folderPath)
 
-  # 刪除子mp4
-  deleteMp4(folderPath)
+    # 取得封面
+    getCover(html_file=dr.page_source, folder_path=folderPath)
 
-  # 取得封面
-  getCover(html_file=dr.page_source, folder_path=folderPath)
-
-  # 轉檔
-  ffmpegEncode(folderPath, dirName, encode)
+    # 轉檔
+    ffmpegEncode(folderPath, dirName, encode)
